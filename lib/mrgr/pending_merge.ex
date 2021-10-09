f0e5be15e7416e7d319dceda55438a7478921ce6
Desmond Bowe <desmond@crevalle.io>
2021-10-09 10:14:08 -0700

    Pending merge context file

---
+++ lib/mrgr/pending_merge.ex
@@ -0,0 +1,14 @@
+defmodule Mrgr.PendingMerge do
+
+
+  def create_first_linky_comment(repo, pr, token) do
+
+    # crevalle/mrgr => ["crevalle", "mrgr"]
+    [owner, repo_name] = String.split(repo.full_name, "/")
+
+    comment = %{body: "hi desmond!"}
+
+    client = Tentacat.Client.new(%{access_token: token})
+    response = Tentacat.Issues.Comments.create(client, owner, repo_name, pr.number, comment)
+  end
+end
