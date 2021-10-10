8f67b21125d5e762d7fb3fcb9a5da8ae357b4520
Desmond Bowe <desmond@crevalle.io>
2021-10-10 09:34:24 -0700

    Fix name

--- lib/mrgr/github/webhook.ex
+++ lib/mrgr/github/webhook.ex
@@ -3,43 +3,43 @@ defmodule Mrgr.Github.Webhook do
     Dispatcher that receives webhooks and figures out what to do with them.
   """
 
-  def process(%{"installation" => _params, "action" => "created"} = payload) do
+  def handle(%{"installation" => _params, "action" => "created"} = payload) do
     Mrgr.Installation.create_from_webhook(payload)
   end
 
-  def process(%{"installation" => _params, "action" => "deleted"} = payload) do
+  def handle(%{"installation" => _params, "action" => "deleted"} = payload) do
     Mrgr.Installation.delete_from_webhook(payload)
   end
 
-  def process(%{"installation" => _params, "action" => "requested"} = payload) do
+  def handle(%{"installation" => _params, "action" => "requested"} = payload) do
     payload
   end
 
-  def process(%{"pull_request" => _params, "action" => "opened" } = payload) do
+  def handle(%{"pull_request" => _params, "action" => "opened" } = payload) do
     payload
   end
 
-  def process(%{"pull_request" => _params, "action" => "closed", "merged" => true} = payload) do
+  def handle(%{"pull_request" => _params, "action" => "closed", "merged" => true} = payload) do
     # merged
     payload
   end
 
-  def process(%{"pull_request" => _params, "action" => "closed", "merged" => false} = payload) do
+  def handle(%{"pull_request" => _params, "action" => "closed", "merged" => false} = payload) do
     # closed, not merged
     payload
   end
 
   # HEAD OF PR IS UPDATED - create a new check suite/run, new checklist
-  def process(%{"action" => "synchronize", "pull_request" => _params} = payload) do
+  def handle(%{"action" => "synchronize", "pull_request" => _params} = payload) do
     IO.inspect("GOT SYNCHRONIZE")
     payload
   end
 
-  def process(%{"action" => "requested", "check_suite" => _params} = payload) do
+  def handle(%{"action" => "requested", "check_suite" => _params} = payload) do
     Mrgr.CheckRun.process(payload)
   end
 
   # suspended?
-  def process(payload), do: payload
+  def handle(payload), do: payload
 
 end
