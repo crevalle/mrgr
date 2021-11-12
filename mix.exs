86c9172896fb6d7924710ed77d002874eb890a30
Desmond Bowe <desmond@crevalle.io>
2021-11-11 18:35:56 -0800

    Bump deps - add tzdata and get liveview mount hooks

--- mix.exs
+++ mix.exs
@@ -45,13 +45,14 @@ defmodule Mrgr.MixProject do
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_dashboard, "~> 0.5"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.16.0"},
+      {:phoenix_live_view, "~> 0.17.0"},
       {:plug_cowboy, "~> 2.5"},
       {:postgrex, ">= 0.0.0"},
       {:swoosh, "~> 1.3"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:tentacat, "~> 2.0"},
+      {:tzdata, "~> 1.1"},
       {:ueberauth_github, "~> 0.8"}
     ]
   end
