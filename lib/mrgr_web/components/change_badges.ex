5c28e4e0616fe31437604ecc2abf1ef74ead423a
Desmond Bowe <desmond@crevalle.io>
2022-07-18 22:11:24 -0700

    router and deps changes, plus component badges

--- lib/mrgr_web/components/change_badges.ex
+++ lib/mrgr_web/components/change_badges.ex
@@ -5,12 +5,27 @@ defmodule MrgrWeb.Component.PendingMerge do
 
   def change_badges(assigns) do
     ~H"""
-      <%= if has_migration?(assigns.merge) do %>
-        <p class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0 sm:ml-6">
-          <p class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">migration</p>
-        </p>
-      <% end %>
+      <div class="mt-2 flex items-center space-x-2 text-sm text-gray-500 sm:mt-0 sm:ml-6">
+        <%= if has_migration?(assigns.merge) do %>
+          <.badge bg="bg-green-100" text="text-green-800">migration</.badge>
+        <% end %>
 
+        <%= if router_changed?(assigns.merge) do %>
+          <.badge bg="bg-blue-100" text="text-blue-800">router</.badge>
+        <% end %>
+
+        <%= if dependencies_changed?(assigns.merge) do %>
+          <.badge bg="bg-yellow-100" text="text-yellow-800">dependencies</.badge>
+        <% end %>
+      </div>
+    """
+  end
+
+  def badge(%{bg: _, text: _} = assigns) do
+    ~H"""
+      <p class={"px-2 inline-flex text-xs leading-5 font-semibold rounded-full #{@bg} #{@text}"}>
+        <%= render_slot(@inner_block) %>
+      </p>
     """
   end
 
@@ -21,5 +36,16 @@ defmodule MrgrWeb.Component.PendingMerge do
     end)
   end
 
+  def router_changed?(%{files_changed: files}) do
+    Enum.any?(files, fn f ->
+      String.ends_with?(f, "router.ex")
+    end)
+  end
+
+  def dependencies_changed?(%{files_changed: files}) do
+    Enum.any?(files, fn f ->
+      String.ends_with?(f, "mix.exs")
+    end)
+  end
 
 end
