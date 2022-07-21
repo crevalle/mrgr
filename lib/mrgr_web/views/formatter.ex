2d905acba7cfe37b3520fd2621b96fec4167d6c8
Desmond Bowe <desmond@crevalle.io>
2022-07-21 10:46:54 -0700

    Human times ago for activity events

--- lib/mrgr_web/views/formatter.ex
+++ lib/mrgr_web/views/formatter.ex
@@ -16,11 +16,41 @@ defmodule MrgrWeb.Formatter do
     Calendar.strftime(timestamp, "%I:%M%p %b %d, '%y")
   end
 
-  def ago(timestamp, local_timezone) do
-    # TODO: make this something like 3h, 2d.  but it should update constantly as time passes.
-    # for now, just pass through to ordinary long form timestamp formatting
-    #
-    ts(timestamp, local_timezone)
+  def ago(timestamp) do
+    seconds = DateTime.diff(DateTime.utc_now(), timestamp)
+
+    case seconds do
+      s when s < 60 ->
+        "<1m"
+
+      s when s < 3600 ->
+        "#{floor(s / 60)}m"
+
+      s when s < 86400 ->
+        "#{floor(s / 3600)}h"
+
+      # 2 weeks
+      s when s < 1_209_600 ->
+        "#{floor(s / 86400)}d"
+
+      # 8 weeks
+      s when s < 4_838_400 ->
+        "#{floor(s / (86400 * 7))}w"
+
+      # hack because 8 weeks does not exactly equal 2 months
+      s when s < 5_184_000 ->
+        %{month: m} = DateTime.utc_now()
+        timestamp.month
+        "#{m - timestamp.month}mo"
+
+      # less than 2 years, roughly
+      s when s < 62_208_000 ->
+        "#{floor(s / (86400 * 30))}mo"
+
+      # 2 years or more
+      s ->
+        "#{round(s / (86400 * 30 * 12))}y"
+    end
   end
 
   def ref("refs/heads/" <> name), do: name
