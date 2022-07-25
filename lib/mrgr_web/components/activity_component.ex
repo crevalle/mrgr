1e0b6398409fa80e978294a9a80c9908207fb2c6
Desmond Bowe <desmond@crevalle.io>
2022-07-25 11:55:49 -0700

    Helpful icons in activity stream

--- lib/mrgr_web/components/activity_component.ex
+++ lib/mrgr_web/components/activity_component.ex
@@ -18,6 +18,12 @@ defmodule MrgrWeb.Components.ActivityComponent do
         <%= translate_merge_action(action) %> <%= @payload.title %>
       </:description>
 
+      <:icon>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z" clip-rule="evenodd" />
+        </svg>
+      </:icon>
+
       <:detail>
         <%= MrgrWeb.Component.PendingMerge.change_badges(%{merge: @payload}) %>
       </:detail>
@@ -32,6 +38,11 @@ defmodule MrgrWeb.Components.ActivityComponent do
         pushed <%= shorten_sha(@payload["after"]) %> to <%= ref(@payload["ref"]) %>
       </:description>
 
+      <:icon>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z" />
+        </svg>
+      </:icon>
       <:detail>
         <%= @payload["head_commit"]["message"] %>
       </:detail>
@@ -43,7 +54,10 @@ defmodule MrgrWeb.Components.ActivityComponent do
     ~H"""
     <li class="py-4">
       <div class="flex space-x-3">
-        <img class="h-6 w-6 rounded-full" src={"#{@avatar_url}"} alt="">
+        <div class="flex flex-col space-y-1">
+          <img class="h-6 w-6 rounded-full" src={"#{@avatar_url}"} alt="">
+          <p class="text-sm text-gray-500"><%= render_slot(@icon) %></p>
+        </div>
         <div class="flex-1 space-y-1">
           <div class="flex items-center justify-between">
             <h3 class="text-sm font-medium"><%= @name %></h3>
