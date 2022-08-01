3c564103e113884bf83b06ff9d0a6e8c4cbfdd11
Desmond Bowe <desmond@crevalle.io>
2022-07-31 22:02:40 -0700

    Some weird bug prevented button colors from showign up

--- lib/mrgr_web/components/ui.ex
+++ lib/mrgr_web/components/ui.ex
@@ -21,7 +21,7 @@ defmodule MrgrWeb.Components.UI do
     color = assigns[:color] || "emerald"
 
     class =
-      "ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-#{color}-600 hover:bg-#{color}-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-#{color}-500"
+      "inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-#{color}-600 hover:bg-#{color}-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-#{color}-500"
 
     type = if assigns[:submit], do: "submit", else: false
 
