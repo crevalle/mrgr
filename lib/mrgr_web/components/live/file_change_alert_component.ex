964ea074ad746a1ebd92a3b7d51d51faeb83aa75
Desmond Bowe <desmond@crevalle.io>
2022-10-12 11:27:25 -0700

    Checkpoint - three-columns for file change alerts, componentize stuff

---
+++ lib/mrgr_web/components/live/file_change_alert_component.ex
@@ -0,0 +1,3 @@
+defmodule MrgrWeb.Components.Live.FileChangeAlertComponent do
+  use MrgrWeb, :live_component
+end
--- lib/mrgr_web/live/file_change_alert_live.ex
+++ lib/mrgr_web/live/file_change_alert_live.ex
@@ -9,77 +9,62 @@ defmodule MrgrWeb.FileChangeAlertLive do
 
       socket
       |> assign(:current_user, current_user)
+      |> assign(:selected_repo_id, nil)
+      |> assign(:changeset, nil)
       |> assign(:repos, repos)
       |> ok()
     else
-      socket
-      |> assign(:current_user, nil)
-      |> assign(:repos, [])
-      |> ok()
+      ok(socket)
     end
   end
 
-  def render(assigns) do
-    ~H"""
-    <!-- This example requires Tailwind CSS v2.0+ -->
-    <div class="px-4 sm:px-6 lg:px-8">
-      <.heading title="File Change Alerts" description="Add alerts based on custom file or folder names." />
-      <div class="mt-8 flex flex-col">
-        <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-              <table class="min-w-full">
-                <thead class="bg-white">
-                  <tr>
-                    <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Pattern</th>
-                    <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Label</th>
-                    <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                      <span class="sr-only">Edit</span>
-                      <span class="sr-only">Delete</span>
-                    </th>
-                  </tr>
-                </thead>
-                <tbody class="bg-white">
-                  <%= for repo <- @repos do %>
-                    <tr class="border-t border-gray-200">
-                      <th colspan="2" scope="colgroup" class="bg-gray-50 px-4 py-2 text-left text-sm font-semibold text-gray-900 sm:px-6">
-                        <%= repo.name %>
-                      </th>
-                      <th colspan="1" scope="colgroup" class="bg-gray-50 px-4 py-2 text-right text-sm font-semibold text-gray-900 sm:px-6">
-                        <%= link "Edit Alerts", to: Routes.file_change_alert_path(MrgrWeb.Endpoint, :edit, repo.name), class: "text-emerald-600 hover:text-emerald-900 hover:bg-emerald-50 group flex items-center px-2 py-2 text-sm font-medium rounded-md" %>
-                      </th>
-                    </tr>
+  def handle_event("open-add-form", %{"repo-id" => id}, socket) do
+    socket
+    |> assign(:selected_repo_id, id)
+    |> assign(:changeset, empty_changeset())
+    |> noreply()
+  end
 
-                    <%= for alert <- repo.file_change_alerts do %>
-                      <tr class="border-t border-gray-300">
-                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6"><pre>'<%= alert.pattern %>'</pre></td>
-                        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                          <MrgrWeb.Components.Merge.file_change_badge bg={alert.bg_color}>
-                            <%= alert.badge_text %>
-                          </MrgrWeb.Components.Merge.file_change_badge>
-                        </td>
-                        <td>
-                          <%= if alert.notify_user do %>
-                            🟢 <span class="text-gray-400 text-sm italic">you will be notified of new Merges</span>
-                          <% else %>
-                            ⭕️ <span class="text-gray-400 text-sm italic">you will not be notified of new Merges</span>
-                          <% end %>
-                        </td>
-                        <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                        </td>
-                      </tr>
+  def handle_event("form-change", %{"file_change_alert" => params}, socket) do
+    changeset =
+      %Mrgr.Schema.FileChangeAlert{}
+      |> build_changeset(params)
 
-                    <% end %>
-                  <% end %>
+    socket
+    |> assign(:cs, changeset)
+    |> noreply()
+  end
 
-                  <!-- More people... -->
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    """
+  def handle_event("save-file-alert", %{"file_change_alert" => params}, socket) do
+    params =
+      params
+      |> Map.put("repository_id", socket.assigns.selected_repo_id)
+      |> Map.put("source", :user)
+
+    params
+    |> Mrgr.FileChangeAlert.create()
+    |> case do
+      {:ok, alert} ->
+        new_alert = {alert, build_changeset(alert)}
+        alerts = [new_alert | socket.assigns.alerts]
+
+        socket
+        |> assign(:selected_repo_id, nil)
+        |> assign(:cs, nil)
+        |> noreply()
+
+      {:error, cs} ->
+        socket
+        |> assign(:cs, cs)
+        |> noreply()
+    end
+  end
+
+  defp build_changeset(schema, params \\ %{}) do
+    Mrgr.Schema.FileChangeAlert.changeset(schema, params)
+  end
+
+  defp empty_changeset do
+    build_changeset(%Mrgr.Schema.FileChangeAlert{})
   end
 end
