e17377a2e15f46d406fa13d6b5fbb643092bd205
Desmond Bowe <desmond@crevalle.io>
2022-11-21 13:55:02 -0800

    We pull the policy off the repo now

--- lib/mrgr/worker/enforce_repository_settings.ex
+++ lib/mrgr/worker/enforce_repository_settings.ex
@@ -4,10 +4,9 @@ defmodule Mrgr.Worker.EnforceRepositorySettings do
   @impl Oban.Worker
   def perform(%Oban.Job{args: %{"policy_id" => id, "repo_id" => repo_id}}) do
     policy = Mrgr.RepositorySettingsPolicy.find(id)
-    # see below.  need this for broadcasting to the live view
     repository = %{Mrgr.Repository.find(repo_id) | policy: policy}
 
-    Mrgr.Repository.enforce_repo_policy(repository, policy)
+    Mrgr.Repository.enforce_repo_policy(repository)
 
     :ok
   end
@@ -19,11 +18,9 @@ defmodule Mrgr.Worker.EnforceRepositorySettings do
     # get smarter about this but i don't want to risk overloading the
     # connection pool
     Enum.map(policy.repositories, fn repo ->
-      # ends up getting broadcast to the RepoListLiveView which
-      # expects policy to be preloaded
       repo = %{repo | policy: policy}
 
-      Mrgr.Repository.enforce_repo_policy(repo, policy)
+      Mrgr.Repository.enforce_repo_policy(repo)
     end)
 
     :ok
