288e615082369ce377800882ca34c74fc68df1e7
Desmond Bowe <desmond@crevalle.io>
2023-01-05 11:44:08 -0800

    got our domain working baby

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ config :mrgr, :env, "prod"
 # which you should run after static files are built and
 # before starting your production server.
 config :mrgr, MrgrWeb.Endpoint,
-  url: [scheme: "https", host: "mrgr.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "mrgr.io", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
