d3e2230f3bfa2016ced6f509dbff5ecb433af729
Desmond Bowe <desmond@crevalle.io>
2023-02-01 13:44:59 -0800

    Fix option name

--- lib/mrgr/worker/github_webhook.ex
+++ lib/mrgr/worker/github_webhook.ex
@@ -1,5 +1,5 @@
 defmodule Mrgr.Worker.GithubWebhook do
-  use Oban.Worker, max_attemps: 1
+  use Oban.Worker, max_attempts: 1
 
   @impl Oban.Worker
   def perform(%Oban.Job{args: %{"id" => id}}) do
