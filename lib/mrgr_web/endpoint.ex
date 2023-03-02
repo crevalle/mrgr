d8aa264cbd1c42e81e1fd3ba7c158abc3f2a602a
Desmond Bowe <desmond@crevalle.io>
2023-03-02 11:49:23 -0800

    Bump deps, no longer need this appsignal line

--- lib/mrgr_web/endpoint.ex
+++ lib/mrgr_web/endpoint.ex
@@ -1,6 +1,5 @@
 defmodule MrgrWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :mrgr
-  use Appsignal.Phoenix
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
