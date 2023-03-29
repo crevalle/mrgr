6172ada9b96ec8cbc89ffe17c7b82b7ac356400d
Desmond Bowe <desmond@crevalle.io>
2023-03-28 21:22:35 -0700

    Dormancy respects weekends
    * monday
      fresh threshold (opened friday) - 72 hours
      stale threshold (opened wednesday) - 120 hours
    
    * tuesday
      fresh threshold (opened monday) - 24 hours
      stale threshold (opened thursday) - 120 hours
    
    * wednesday
      fresh threshold (opened tuesday) - 24 hours
      stale threshold (opened friday) - 120 hours
    
    * thursday
      fresh threshold (opened wednesday) - 24 hours
      stale threshold (opened monday) - 72 hours
    
    * friday
      fresh threshold (opened thursday) - 24 hours
      stale threshold (opened tuesday) - 72 hours
    
    on weekends nothing becomes dormant or stale
    * saturday
      fresh threshold (opened thursday) - 48 hours
      stale threshold (opened tuesday) - 96 hours
    
    * sunday
      fresh threshold (opened thursday) - 72 hours
      stale threshold (opened tuesday) - 120 hours

--- lib/mrgr/pull_request/dormant.ex
+++ lib/mrgr/pull_request/dormant.ex
@@ -30,14 +30,39 @@ defmodule Mrgr.PullRequest.Dormant do
   end
 
   def fresh_threshold do
-    Mrgr.DateTime.now()
-    |> DateTime.add(-24, :hour)
-    |> Mrgr.DateTime.safe_truncate()
+    now = Mrgr.DateTime.now()
+    offset = fresh_offset(Date.day_of_week(now))
+
+    set_offset(now, offset)
   end
 
   def stale_threshold do
-    Mrgr.DateTime.now()
-    |> DateTime.add(-72, :hour)
+    now = Mrgr.DateTime.now()
+    offset = stale_offset(Date.day_of_week(now))
+
+    set_offset(now, offset)
+  end
+
+  defp set_offset(now, offset) do
+    now
+    |> DateTime.add(-offset, :hour)
     |> Mrgr.DateTime.safe_truncate()
   end
+
+  def fresh_offset(1), do: 72
+  def fresh_offset(2), do: 24
+  def fresh_offset(3), do: 24
+  def fresh_offset(4), do: 24
+  def fresh_offset(5), do: 24
+  def fresh_offset(6), do: 48
+  def fresh_offset(7), do: 72
+
+  def stale_offset(1), do: 120
+  def stale_offset(2), do: 120
+  def stale_offset(3), do: 120
+  def stale_offset(4), do: 72
+  def stale_offset(5), do: 72
+  def stale_offset(6), do: 96
+  def stale_offset(7), do: 120
+
 end
