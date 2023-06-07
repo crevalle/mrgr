b8a3b5d9ff1fb113a38dfbb0f709b2ee78ec41e3
Desmond Bowe <desmond@crevalle.io>
2023-06-07 12:31:49 -0400

    WIP - mechanics of finding dormancy and triggering notices
    TODO:
    
    * turn on scheduleing, play nicely wiht which PRs are queried
    * notification formatting

--- lib/mrgr/pull_request/dormant.ex
+++ lib/mrgr/pull_request/dormant.ex
@@ -1,51 +1,44 @@
 defmodule Mrgr.PullRequest.Dormant do
-  # def notify(pull_request, user) do
-  # pull_request = mark_dormancy_notified(pull_request)
+  use Mrgr.Notification.Event
 
-  # notify_consumers(pull_request)
-  # end
+  @spec notify_consumers(Mrgr.Schema.PullRequest.t()) :: Mrgr.Notification.result()
+  def notify_consumers(pull_request) do
+    consumers = fetch_consumers(pull_request)
 
-  # def notify_consumers(pull_request) do
-  # consumers = fetch_consumers(pull_request)
+    Enum.reduce(consumers, %{}, fn {channel, recipients}, acc ->
+      results = notify_channel(channel, recipients, pull_request)
 
-  # email = Enum.map(consumers.email, fn recipient ->
-  # send_dormant_email(recipient, pull_request)
-  # end)
+      Map.put(acc, channel, results)
+    end)
+  end
 
-  # slack = Enum.map(consumers.slack, fn recipient ->
-  # send_dormant_slack(recipient, pull_request)
-  # end)
+  def notify_channel(:email, recipients, pull_request) do
+    Enum.map(recipients, fn recipient ->
+      send_email(recipient, pull_request)
+    end)
+  end
 
-  # %{email: email, slack: slack}
-  # end
+  def notify_channel(:slack, recipients, pull_request) do
+    Enum.map(recipients, fn recipient ->
+      send_slack_message(recipient, pull_request)
+    end)
+  end
 
-  # def mark_dormancy_notified(pull_request) do
-  # pull_request
-  # |> Ecto.Changeset.change(%{dormancy_notified: true})
-  # |> Mrgr.Repo.update!()
-  # end
+  def send_email(recipient, pull_request) do
+    email = Mrgr.Email.dormant_pr(recipient, pull_request)
 
-  # def mark_dormancy_reset(pull_request) do
-  # pull_request
-  # |> Ecto.Changeset.change(%{dormancy_notified: false})
-  # |> Mrgr.Repo.update!()
-  # end
+    Mrgr.Mailer.deliver_and_log(email, @dormant_pr)
+  end
 
-  # def send_dormant_email(recipient, pull_request) do
-  # email = Mrgr.Email.dormant_pr(recipient, pull_request)
+  def send_slack_message(recipient, pull_request) do
+    message = Mrgr.Slack.Message.DormantPR.render(pull_request, recipient)
 
-  # Mrgr.Mailer.deliver(email)
-  # end
+    Mrgr.Slack.send_and_log(message, recipient, @dormant_pr)
+  end
 
-  # def send_dormant_slack(recipient, pull_request) do
-  # message = Mrgr.Slack.Message.DormantPR.render(pull_request, recipient)
-
-  # Mrgr.Slack.send_message(message, recipient)
-  # end
-
-  # def fetch_consumers(pull_request) do
-  # Mrgr.Notification.consumers_of_event(Mrgr.Notification.Event.pr_dormant(), pull_request)
-  # end
+  def fetch_consumers(pull_request) do
+    Mrgr.Notification.consumers_of_event(@dormant_pr, pull_request)
+  end
 
   def dormant?(%Mrgr.Schema.PullRequest{} = pr, timezone) do
     dormant?(pr.last_activity_at, timezone)
